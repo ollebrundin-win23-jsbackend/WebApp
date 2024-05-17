46d088ee7a77e10a864955889638fdf3f58c48fb
Dyssling <olle.lundgarden@hotmail.com>
2024-05-17 17:35:15 +0200

    SignUp page finished (without functionality) TODO: Styling

---
+++ NewAssignment/NewAssignmentWebApp/NewAssignmentWebApp/Models/SignUpModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewAssignmentWebApp.Models
+{
+    public class SignUpModel
+    {
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "You must enter your first name.")]
+        [MinLength(2, ErrorMessage = "You must enter a valid first name.")]
+
+        public string FirstName { get; set; } = null!;
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "You must enter your last name.")]
+        [MinLength(2, ErrorMessage = "You must enter a valid last name.")]
+
+        public string LastName { get; set; } = null!;
+
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "You must enter your email address.")]
+        [RegularExpression("^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,})+$", ErrorMessage = "You must enter a valid email address.")]
+
+        public string Email { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "You must enter a password.")]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$", ErrorMessage = "Password must be a minimum of 8 characters, and contain at least one number, one lowercase character, one uppercase character and one special character.")]
+
+        public string Password { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "You must confirm your password.")]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}

abdde35b209fab8b20e71d2e518a1ae65344e559
Dyssling <olle.lundgarden@hotmail.com>
2024-05-17 17:16:53 +0200

    SignIn page finished (without sign in functionality)

---
+++ NewAssignment/NewAssignmentWebApp/NewAssignmentWebApp/Models/SignInModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewAssignmentWebApp.Models
+{
+    public class SignInModel
+    {
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "You must enter your email address.")]
+        [RegularExpression("^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,})+$", ErrorMessage = "You must enter a valid email address.")]
+
+        public string Email { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "You must enter a password.")]
+
+        public string Password { get; set; } = null!;
+
+        public bool RememberMe { get; set; } = false;
+    }
+}

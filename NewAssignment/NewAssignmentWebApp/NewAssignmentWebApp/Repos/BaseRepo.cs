4c0f3ef0d2a41387eaf95c60c2e643ecc2f971db
Dyssling <olle.lundgarden@hotmail.com>
2024-05-20 14:10:04 +0200

    SignIn functionality finished. Also added RedirectToAccountPage component to make sure authorized users cannot access SignIn or SignUp pages.

---
+++ NewAssignment/NewAssignmentWebApp/NewAssignmentWebApp/Repos/BaseRepo.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace NewAssignmentWebApp.Repos
+{
+    public abstract class BaseRepo<TEntity, TContext>
+            where TEntity : class
+            where TContext : DbContext
+    {
+        private readonly TContext _context;
+        public BaseRepo(TContext context)
+        {
+            _context = context;
+        }
+
+        public virtual async Task<bool> CreateAsync(TEntity entity)
+        {
+            try
+            {
+                _context.Set<TEntity>().Add(entity);
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public virtual async Task<TEntity> GetOneAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            try
+            {
+                var entity = await _context.Set<TEntity>().FirstOrDefaultAsync(expression);
+                if (entity != null)
+                {
+                    return entity;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return null!;
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            try
+            {
+                var entityList = await _context.Set<TEntity>().ToListAsync();
+                return entityList;
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null!;
+            }
+        }
+
+        public virtual async Task<bool> UpdateAsync(Expression<Func<TEntity, bool>> expression, TEntity entity)
+        {
+            try
+            {
+                var currentEntity = await _context.Set<TEntity>().FirstOrDefaultAsync(expression);
+                if (currentEntity != null)
+                {
+                    _context.Entry(currentEntity).CurrentValues.SetValues(entity);
+                    await _context.SaveChangesAsync();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return false;
+        }
+
+        public virtual async Task<bool> DeleteAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            try
+            {
+                var entity = await _context.Set<TEntity>().FirstOrDefaultAsync(expression);
+                if (entity != null)
+                {
+                    _context.Set<TEntity>().Remove(entity);
+                    await _context.SaveChangesAsync();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return false;
+        }
+    }
+}
---
+++ NewAssignment/NewAssignmentWebApp/NewAssignmentWebApp/Repos/UserRepo.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewAssignmentWebApp.Data;
+using System.Linq.Expressions;
+
+namespace NewAssignmentWebApp.Repos
+{
+    public class UserRepo : BaseRepo<ApplicationUser, ApplicationDbContext>
+    {
+        private readonly ApplicationDbContext _context;
+        public UserRepo(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        //I nedanstående overrides  gör jag en Include för att göra en JOIN sats på User entiteterna man får tillbaka, så att man även får tillbaka dess addressinformation.
+
+        public override async Task<IEnumerable<ApplicationUser>> GetAllAsync()
+        {
+            try
+            {
+                var entityList = await _context.Users.Include(x => x.Address).ToListAsync();
+                return entityList;
+
+            }
+            catch { }
+
+            return null!;
+        }
+
+        public override async Task<ApplicationUser> GetOneAsync(Expression<Func<ApplicationUser, bool>> expression)
+        {
+            try
+            {
+                var entity = await _context.Users.Include(x => x.Address).FirstOrDefaultAsync(expression);
+                if (entity != null)
+                {
+                    return entity;
+                }
+            }
+            catch { }
+
+            return null!;
+        }
+    }
+}

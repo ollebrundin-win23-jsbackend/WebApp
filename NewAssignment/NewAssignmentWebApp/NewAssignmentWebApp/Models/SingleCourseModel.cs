c06b25169fe25de1f9b48c988102d2c7ea85bcfa
Dyssling <olle.lundgarden@hotmail.com>
2024-05-24 23:12:34 +0200

    SingleCourse page finished. Typo in Courses page title corrected.

---
+++ NewAssignment/NewAssignmentWebApp/NewAssignmentWebApp/Models/SingleCourseModel.cs
@@ -0,0 +1,19 @@
+﻿namespace NewAssignmentWebApp.Models
+{
+    public class SingleCourseModel
+    {
+        public string Title { get; set; } = null!;
+        public string HeaderDescription { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public decimal Price { get; set; }
+        public decimal? DiscountedPrice { get; set; }
+        public int Hours { get; set; }
+        public int? Likes { get; set; }
+        public int? LikePercentage { get; set; }
+        public bool IsBestSeller { get; set; } = false;
+        public int? Rating { get; set; }
+        public int? Reviews { get; set; }
+        public string ImageUrl { get; set; } = null!;
+    }
+}
